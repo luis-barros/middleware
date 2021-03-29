19858b479c97c0d6359e4e7a4cad85c5440daa2c
luigi.barros@gmail.com <Lfbs0509>
2021-03-29 09:30:52 +0200

    Make sayHello abstract

--- src/main/kotlin/com/cyrius/middleware/model/BaseTable.kt
+++ src/main/kotlin/com/cyrius/middleware/model/BaseTable.kt
@@ -4,6 +4,6 @@ import javax.persistence.*
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-open class BaseTable(@Id open var id: Int, open var ready: Boolean, open var name: String) {
-    open fun sayHello() {}
+abstract class BaseTable(@Id open var id: Int, open var ready: Boolean, open var name: String) {
+    abstract fun sayHello()
 }
